531e91d88eb13c792eb5a524fde67ecd6af48775
Michael DESIGAUD <michaeldesigaud@gmail.com>
2016-05-01 12:14:56 +0200

    Add icon and splash + fix current song playing

--- app/services/media.service.ts
+++ app/services/media.service.ts
@@ -26,33 +26,48 @@ export class MediaService {
         console.log('Stop current media',this.media);
         this.media.currentTime = 0;
     }
+    isPlaying():boolean {
+        return this.media && this.media.duration > 0 &&!this.media.paused;
+    }
+    isCurrentSound(newSound:any):boolean {
+        return this.currentSound && newSound.id === this.currentSound.id;
+    }
     play(newSound:any):void {
         console.log('Start sound',newSound);
-        if(this.media && !this.media.paused) {
-            this.stop();
-        }
-        this.currentSound = newSound;
 
-        if(this.platform.is('cordova')) {
-            console.log('cordova');
-            if(!this.cacheService.soundInCache(newSound.file)) {
-                this.media.src = newSound.file;
-            } else {
-                console.log('Playing sound from cache',this.cacheService.getCachedSoundPath(newSound));
-                this.media.src = this.cacheService.getCachedSoundPath(newSound);
+        let soundPlaying:boolean = this.isPlaying() && this.isCurrentSound(newSound);
+
+        if(!soundPlaying) {
+            if (this.media && !this.media.paused) {
+                this.stop();
             }
+            this.currentSound = newSound;
+
+            if (this.platform.is('cordova')) {
+                console.log('cordova');
+                if (!this.cacheService.soundInCache(newSound.file)) {
+                    this.media.src = newSound.file;
+                } else {
+                    console.log('Playing sound from cache', this.cacheService.getCachedSoundPath(newSound));
+                    this.media.src = this.cacheService.getCachedSoundPath(newSound);
+                }
+            } else {
+                if (!this.cacheService.soundInCache(newSound.file)) {
+                    this.media.src = newSound.file;
+                } else {
+                    console.log('Playing sound from cache', this.cacheService.soundCache.toInternalURL(newSound.file));
+                    this.media.src = this.cacheService.soundCache.toInternalURL(newSound.file);
+                }
+            }
+
+            this.media.load();
+            this.media.play();
+
+            this.lastSound = newSound;
         } else {
-            if(!this.cacheService.soundInCache(newSound.file)) {
-                this.media.src = newSound.file;
-            } else {
-                console.log('Playing sound from cache',this.cacheService.soundCache.toInternalURL(newSound.file));
-                this.media.src = this.cacheService.soundCache.toInternalURL(newSound.file);
-            }
+            console.log('This song is playing already');
+            this.currentSound.loading = false;
+            this.currentSound.playing = true;
         }
-
-        this.media.load();
-        this.media.play();
-
-        this.lastSound = newSound;
     }
 }
